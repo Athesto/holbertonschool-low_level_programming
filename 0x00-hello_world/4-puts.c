f71dd69dc8c6d3540370c63d013f84574560e63a
Athesto <gamez.live@gmail.com>
2020-06-10 17:29:18 -0500

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - 4-put
+ * @parameterx: x
+ * Description:
+ * Return:  0 ok
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
