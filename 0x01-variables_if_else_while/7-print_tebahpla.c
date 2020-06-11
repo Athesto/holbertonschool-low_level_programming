b224dfac23faf653fa8c2e584b3b458f989025c2
Athesto <gamez.live@gmail.com>
2020-06-11 13:52:39 -0500

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - 7-print reverse alphabet
+ * main - 7-printl_lowalpha_reversed
  * Return: 0 Always
  */
 int main(void)
 {
 	int c;
 
-	for (c = 'z' ; 'a' <= c ; c--)
+	for (c = 'z' ; c >= 'a' ; c--)
 		putchar(c);
 	putchar('\n');
 	return (0);
