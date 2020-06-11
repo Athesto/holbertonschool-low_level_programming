5f8b511fcb3cea9ea14c956fb123d47b5e3a40a1
Athesto <gamez.live@gmail.com>
2020-06-10 22:22:15 -0500

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - 9-UNIX
+ * Description: No man 3 printf
+ * Return: return sometimes 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n", 60);
+
+	return (1);
+}
