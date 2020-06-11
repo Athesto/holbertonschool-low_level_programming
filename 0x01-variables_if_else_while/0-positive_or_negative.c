0ad2cb360c2dced2bea243604d307877ab8ad487
Athesto <gamez.live@gmail.com>
2020-06-11 11:34:28 -0500

    fixing spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,9 +12,9 @@ int main(void)
 {
 		int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					printf("%d is %s", n, n > 0 ? "positive" : "negative");
-					return (0);
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+		printf("%d is %s", n, n > 0 ? "positive" : "negative");
+		return (0);
 }
