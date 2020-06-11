1b5737ff76e9a5090600804dd10b5cfcce5e1fc2
Athesto <gamez.live@gmail.com>
2020-06-10 20:14:28 -0500

    remove long alone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of a int: %li byte(s)\n", sizeof(int));
-	printf("Size of a long: %li byte(s)\n", sizeof(long));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("Size of a long long: %li byte(s)\n", sizeof(long long));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
