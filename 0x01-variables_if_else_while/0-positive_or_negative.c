00034544478648063a00fece029a7afef647b56f
Athesto <gamez.live@gmail.com>
2020-06-11 10:12:05 -0500

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - 1-Describe random
+ * Description: show if random is pos or neg
+ * Return: 0, Always
+ */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+					printf("%d is %s", n, n > 0 ? "positive" : "negative");
+					return (0);
+}
