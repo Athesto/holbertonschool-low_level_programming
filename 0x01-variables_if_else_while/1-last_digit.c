e0da4840161bb8e07640d2ac3e97fdadadbee899
Athesto <gamez.live@gmail.com>
2020-06-11 10:52:08 -0500

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - 1-Describe random
+ * Description: show if random is pos or neg
+ * Return: 0, Always
+ */
+int main(void)
+{
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		/* your code goes there */
+		int last = n % 10;
+
+		printf("Last digit of %d is %d ", n, last);
+		if (last > 5)
+			printf("and is greater than 5\n");
+		else if (!last)
+			printf("and is 0\n");
+		else
+			printf("and is less than 6 and not 0\n");
+		return (0);
+}
