aeca24d6df06d7d56a1ecb0719856b12ccfd3314
Athesto <gamez.live@gmail.com>
2020-06-10 20:24:25 -0500

    checked output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
-	printf("Size of a int: %li byte(s)\n", sizeof(int));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %li byte(s)\n", sizeof(long long));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 
 	return (0);
