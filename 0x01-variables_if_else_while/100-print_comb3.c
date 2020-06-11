1ac1b43225c80bfc4e4e8835778fb9a259994f40
Athesto <gamez.live@gmail.com>
2020-06-11 18:28:44 -0500

    100 restore overwriter file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,24 +6,20 @@
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j  <= 9 ; j++)
 		{
-			for (k = 0 ; k  <= 9 ; k++)
+			if (i == j || i > j)
+				continue;
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i < 8 || j < 9)
 			{
-				if (i >= j || i >= k || j >= k)
-					continue;
-				putchar('0' + i);
-				putchar('0' + j);
-				putchar('0' + k);
-				if (i < 7)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
