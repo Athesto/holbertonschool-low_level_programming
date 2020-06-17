0dd4c11f32d7337a4a302307744b231f5e9d3a0b
Athesto <gamez.live@gmail.com>
2020-06-17 02:29:02 -0500

    added header ifdef

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10();
@@ -10,3 +12,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+#endif
