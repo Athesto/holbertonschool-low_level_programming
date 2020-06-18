f0c469973f853081a3da71aede372b8088586372
Athesto <gamez.live@gmail.com>
2020-06-18 18:29:06 -0500

    added new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
 		_putchar('0' + i);
 	}
-	_putchar('0' + i);
+	_putchar('\n');
 }
