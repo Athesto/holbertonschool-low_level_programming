d9d3569ab4126c099655e07f7b37fa53d5d7112a
Athesto <gamez.live@gmail.com>
2020-06-18 18:26:10 -0500

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - print 0-9 no2,4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar('0' + i);
+	}
+	_putchar('0' + i);
+}
