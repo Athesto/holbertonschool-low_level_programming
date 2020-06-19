7e239cd240bd1f25c373a86b2695502734ed2c9d
Athesto <gamez.live@gmail.com>
2020-06-18 23:32:37 -0500

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+		putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
