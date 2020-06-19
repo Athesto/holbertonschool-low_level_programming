a42aa533c01483e9da7620be420a7014a083b2d6
Athesto <gamez.live@gmail.com>
2020-06-19 00:06:38 -0500

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_triangle - triangle
+ * @size: triangle size
+*/
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col <= size; col++)
+		{
+			if (col < size - row)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
