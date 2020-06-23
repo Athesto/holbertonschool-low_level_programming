a1c778fe807672782479bd743e61323baec42b0c
Athesto <gamez.live@gmail.com>
2020-06-23 13:11:55 -0500

    0x05 added holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,4 +2,9 @@
 #define HOLBERTON_H
 void reset_to_98(int *n);
 int _putchar(char);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif
