284e6ea3b4d8f3cff747b8567aaa9acb3ad17eda
Athesto <gamez.live@gmail.com>
2020-06-24 12:49:40 -0500

    5. using don't changing the pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 /**
  * rev_string - reverse
- *@s: input string
+ * @s: input string
  */
 void rev_string(char *s)
 {
+	int len = 0, fwd, bck;
 	char c;
-	int len = 0;
-	char *p0 = s;
 
-	while (*s)
+	while (*(s + len))
 	{
 		len++;
-		s++;
 	}
-	s--;
-	while (p0 != s)
+	fwd = 0;
+	bck = len - 1; /*len = 9 and last position is 8 from s*/
+
+	while (fwd < bck)
 	{
-		c = *p0;
-		*p0 = *s;
-		*s = c;
-		p0++;
-		s--;
+		c = *(s + fwd);
+		*(s + fwd) = *(s + bck);
+		*(s + bck) = c;
+		fwd++;
+		bck--;
 	}
 }
+
 
