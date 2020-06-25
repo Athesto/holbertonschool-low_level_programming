915ccf671f02f7a63de727e6d0bd66ceea3aad64
Athesto <gamez.live@gmail.com>
2020-06-25 11:43:35 -0500

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_array - print array
+ * @array: pointer to array
+ * @n: elements
+ */
+void print_array(int *array, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", *(array + i), i < n - 1 ? ", " : "\n");
+	}
+}
+
