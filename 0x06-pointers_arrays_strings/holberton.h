4bfd2e125943d7757e4f4582d2c8d075be7ece6d
Athesto <gamez.live@gmail.com>
2020-06-25 15:50:30 -0500

    holberton.h

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+char *_strcat(char *dest, char *src); /*0.0*/
+char *_strncat(char *dest, char *src, int n);/*1.1*/
+char *_strncpy(char *dest, char *src, int n);/*2.2*/
+int _strcmp(char *s1, char *s2);/*3.3*/
+void reverse_array(int *a, int n);/*4.4*/
+char *string_toupper(char *);/*5.5*/
+char *cap_string(char *);/*6.6*/
+char *leet(char *);/*7.7*/
+char *rot13(char *);/*8.8*/
+void print_number(int n);/*9.100*/
+/*magic*//*10.101*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r);/*11.102*/
+void print_buffer(char *b, int size);/*12.103*/
+
+#endif /*_HOLBERTON_H_*/
