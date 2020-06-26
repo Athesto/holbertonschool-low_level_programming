f4c147b6d718c5456577ba52a224d9cadf5d86bd
Athesto <gamez.live@gmail.com>
2020-06-26 00:02:37 -0500

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,5 +5,16 @@
  */
 char *leet(char *str)
 {
+	int i, k;
+	char *A = "aeotlAEOTL", *B = "4307143071";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (k = 0; A[k]; k++)
+		{
+			if (str[i] == A[k])
+				str[i] = B[k];
+		}
+	}
 	return (str);
 }
