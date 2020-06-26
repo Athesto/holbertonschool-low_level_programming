e6db49eb6a9ae4f8b81d4b1d80db81c0d55c3b00
Athesto <gamez.live@gmail.com>
2020-06-25 22:58:01 -0500

    3-check betty

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare strings
+ * @s1:pointer1
+ * @s2:pointer2
+ * Return:integer of comparation
+ */
+int _strcmp(char *s1, char *s2)
+{
+	printf("%s, %s\n", s1, s2);
+	return (0);
+}
