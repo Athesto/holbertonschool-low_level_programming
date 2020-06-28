b6360c0b38ddce9a1d65a0565f08d5f11345a44a
Athesto <gamez.live@gmail.com>
2020-06-27 21:01:07 -0500

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,5 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	printf("%i, %i\n", *a, n);
+	while (n)
+	{
+		printf("%i", a[n]);
+		if (n > 1)
+			printf(", ");
+		else
+			printf("\n");
+		n--;
+	}
 }
