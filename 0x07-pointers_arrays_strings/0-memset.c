2151b6a530a6efcc16f4e7f3615f4be652474a8f
Athesto <gamez.live@gmail.com>
2020-06-28 05:12:27 -0500

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ * _memset - main
+ * @s: output pointer
+ * @b: filling char
+ * @n: number of bytes
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ps = s;
+
+	while (n--)
+		*ps++ = b;
+	return (ps);
+}
