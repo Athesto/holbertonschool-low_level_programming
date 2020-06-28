f7ac79fc822a3bf152dd714b6a4683e252443cfb
Athesto <gamez.live@gmail.com>
2020-06-27 21:45:31 -0500

    4. revese array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,15 @@
  */
 void reverse_array(int *a, int n)
 {
+	int fwd = 0, bck = n - 1; /*pos[0], pos[last]*/
+	int carry;
 
-	while (n)
+	while (fwd < bck)
 	{
-		n--;
-		printf("%i", a[n]);
-		if (n)
-			printf(", ");
+		carry = a[fwd];
+		a[fwd] = a[bck];
+		a[bck] = carry;
+		fwd++;
+		bck--;
 	}
-	printf("\n");
 }
