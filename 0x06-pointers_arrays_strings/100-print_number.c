2641397cabf859cdff307c88f6290b8d83d99627
Athesto <gamez.live@gmail.com>
2020-06-28 02:05:53 -0500

    100-print_number.c

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,10 +1,31 @@
 #include "holberton.h"
+#include <limits.h>
 /**
  * print_number - description
  * @n: intput
  */
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int nabs, zeros = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	nabs =  (unsigned int)n;
+
+	while (nabs / zeros >= 10)
+	{
+		zeros *= 10;
+	}
+	do {
+		_putchar('0' + nabs / zeros);
+		nabs %= zeros;
+		zeros /= 10;
+	} while (nabs);
+
+
+
 }
 
