49ab53426e7cc8362da29b0a39006add0b10df4e
Athesto <gamez.live@gmail.com>
2020-06-29 18:09:48 -0500

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#define NULL ((void *)0)
+/**
+ * _strstr - find subtring
+ * @haystack: input
+ * @neddle: key
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *neddle)
+{
+	char *ps = haystack;
+	int i = 0;
+
+	if (*neddle == '\0')
+		return (NULL);
+
+	while (1)
+	{
+		if (*(ps + i) == *(neddle + i))
+		{
+			i++;
+		}
+		else
+		{
+			ps++;
+			i = 0;
+		}
+		if (neddle[i] == '\0')
+			return (ps);
+	}
+	return (0);
+}
+
+
+
