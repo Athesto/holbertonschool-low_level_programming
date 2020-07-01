eb85359c61d793361f58b144329ea1a6baf562be
Athesto <gamez.live@gmail.com>
2020-07-01 12:51:17 -0500

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - print reverse
+ * @s: input string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
