d38e06e022956c67582452685a34eff26692bf25
Athesto <gamez.live@gmail.com>
2020-07-01 14:06:47 -0500

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - return factorial
+ * @n: input data
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n > 0)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
