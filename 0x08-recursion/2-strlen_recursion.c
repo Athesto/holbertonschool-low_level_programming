eb7120a982e32c7c2424746b384e4e672379e47b
Athesto <gamez.live@gmail.com>
2020-07-01 13:11:40 -0500

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - return len
+ * @s: input string
+ * Return: len
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s)
+		return (_strlen_recursion(s+1) + 1);
+	else
+		return (0);
+}
