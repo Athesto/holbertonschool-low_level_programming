dec6eae7ce50188adee671fdf969c5dac4f6b881
Athesto <gamez.live@gmail.com>
2020-07-05 08:31:20 -0500

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print
+ * @argc: counter
+ * @argv: inputs
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
