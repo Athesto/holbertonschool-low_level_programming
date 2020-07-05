44f56bc4288883c3a7902d53a3a5168d84fa6292
Athesto <gamez.live@gmail.com>
2020-07-05 11:53:56 -0500

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print all values
+ * @argc: counter
+ * @argv: values
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	while (--argc)
+		printf("%s\n", *++argv);
+	return (0);
+}
