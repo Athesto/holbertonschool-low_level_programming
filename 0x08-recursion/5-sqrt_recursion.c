073b47578294f23f708c70c02c9c1e488cbe9062
Athesto <gamez.live@gmail.com>
2020-07-07 06:48:25 -0500

    using static variables

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,5 +6,21 @@
 
 int _sqrt_recursion(int n)
 {
-	return (n);
+	static int n_sta = 1;
+	int n_return;
+
+	if (n_sta * n_sta == n)
+	{
+		n_return = n_sta;
+		n_sta = 1;
+		return (n_return);
+	}
+	else if (n_sta > n / 2)
+	{
+		n_return = n_sta;
+		n_sta = 1;
+		return (-1);
+	}
+	n_sta++;
+	return (_sqrt_recursion(n));
 }
