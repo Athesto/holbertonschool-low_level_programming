2895095d0157144c0fc2cc99f33437dc3dda9c3f
Athesto <gamez.live@gmail.com>
2020-07-07 23:02:35 -0500

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * str_concat - main
  * @s1: input1
@@ -6,8 +7,20 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (*s1)
-		return (s1);
-	else
-		return (s2);
+	char *p;
+	int len1 = 0, len2 = 0;
+	int i;
+
+	while (*(s1 + len1))
+		len1++;
+	while (*(s2 + len2))
+		len2++;
+	p = malloc(sizeof(*p) * len1 + len2 + 1);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		p[i] = s1[i];
+	for (i = 0; i <= len2; i++)
+		p[len1 + i] = s2[i];
+	return (p);
 }
