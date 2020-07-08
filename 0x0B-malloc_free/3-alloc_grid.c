32e0bf06791864f89454f876028e620dfd03f8eb
Athesto <gamez.live@gmail.com>
2020-07-08 00:03:35 -0500

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,9 +7,21 @@
  */
 int **alloc_grid(int width, int height)
 {
-	if (width && height)
-		return (NULL);
-	else
-		return (NULL);
+	int **p;
+	int col, row;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(sizeof(*p) * height);
+	if (!p)
+		return (NULL);
+	for (row = 0; row < height; row++)
+	{
+		p[row] = malloc(sizeof(**p) * width);
+		if (!*p)
+			return (NULL);
+		for (col = 0; col < width; col++)
+			p[row][col] = 0;
+	}
+	return (p);
 }
