f0ddbd6b029474501d41b5078f6ba975e8e11f76
Athesto <gamez.live@gmail.com>
2020-07-07 22:10:00 -0500

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * _strdup - create a new str
+ * @str: source
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int len = 0, i;
+	char *p;
+
+	if (!str)
+		return (NULL);
+	while (*(str + len))
+		len++;
+	p = malloc(sizeof(char) * len);
+	if (!p)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		p[i] = str[i];
+	return (p);
+}
