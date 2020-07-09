d9fd2c0629b095a39bfaf2987e1148d7929f7070
Athesto <gamez.live@gmail.com>
2020-07-09 17:19:27 -0500

    added stdlib
    added string.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdlib.h>
+#include <string.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
