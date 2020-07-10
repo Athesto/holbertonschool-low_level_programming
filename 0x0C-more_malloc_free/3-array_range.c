d7333e565da217af21c3ee4d503033016f6a9b33
Athesto <gamez.live@gmail.com>
2020-07-09 23:30:40 -0500

    include +1 in parenthesis

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	p = malloc(sizeof(*p) * (max - min) + 1);
+	p = malloc(sizeof(*p) * (max - min + 1));
 	if (!p)
 		return (NULL);
 
