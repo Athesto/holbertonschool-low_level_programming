b4338d757f441976df747a5d3102b81e28545c7f
Athesto <gamez.live@gmail.com>
2020-07-13 14:51:24 -0500

    added prototypes of dog.h and typedef of dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* _DOG_H_ */
