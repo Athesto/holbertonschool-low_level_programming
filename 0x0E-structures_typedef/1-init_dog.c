7571f22f4179add274ef4759ed0e234a9c1d1c39
Athesto <gamez.live@gmail.com>
2020-07-13 14:56:34 -0500

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initilize dog
+ * @d: structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
