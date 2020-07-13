80f53e6bf3d48a844ac2921421f4ed3350510d34
Athesto <gamez.live@gmail.com>
2020-07-13 15:38:56 -0500

    added when d is null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initilize dog
  * @d: structure
@@ -8,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		exit(1);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
