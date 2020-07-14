1121e8f71cbd6abb135c4130e67ea445c11bda31
Athesto <gamez.live@gmail.com>
2020-07-13 20:55:25 -0500

    added _putchar

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _putchar(char c);
 #endif /* _DOG_H_ */
