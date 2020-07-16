6d43eee9fc88601f5dfbdff016e9c81c514dde7f
Athesto <gamez.live@gmail.com>
2020-07-15 22:47:04 -0500

    added condition to mod 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,14 +18,14 @@ int main(int c, char *v[])
 		b = atoi(v[3]);
 		p_result = get_op_func(v[2]);
 		if (p_result)
-			if (!(*v[2] == '/' && b == 0))
+			if (!((*v[2] == '/' || *v[2] == '%') && b == 0))
 			{
 				result = p_result(a, b);
 				printf("%d\n", result);
 				return (0);
 			}
 	}
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 
 }
