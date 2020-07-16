be3c489203718320d5e72ee97b8df545a91e896a
Athesto <gamez.live@gmail.com>
2020-07-16 00:56:21 -0500

    print values

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,13 +8,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	char *code = "55 48 89 e5 48 83 ec 20 89 7d ec 48 89 75 e0 83 7d ec 02 75 \
+3e 48 8b 45 e0 48 83 c0 08 48 8b 00 48 89 c7 e8 db fe ff ff \
+89 45 fc 83 7d fc 00 78 11 bf 0a 00 00 00 e8 88 fe ff ff b8 \
+00 00 00 00 eb 20 bf b4 06 40 00 e8 87 fe ff ff b8 02 00 00 \
+00 eb 0f bf b4 06 40 00 e8 76 fe ff ff b8 01 00 00 00 c9 c3 \
+66 2e 0f 1f 84 00 00 00 00 00 0f 1f 44 00 00";
+	int len, i;
 
 	if (argc == 2)
 	{
-		i = atoi(argv[1]);
-		if (i >= 0)
+		len = atoi(argv[1]);
+		if (len >= 0)
 		{
+			len *= 3;
+			i = 0;
+			while(i < len - 1)
+			{
+				putchar(code[i]);
+				i++;
+			}
 			printf("\n");
 			return (0);
 		}
@@ -24,3 +37,6 @@ int main(int argc, char *argv[])
 	printf("Error\n");
 	return (1);
 }
+
+
+
