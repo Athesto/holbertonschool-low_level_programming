256de3746f9a16c9208588d2ebecdd2f100a38d8
Athesto <gamez.live@gmail.com>
2020-07-18 21:48:32 +0000

    print no comma at last value

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(lista, int));
-		if (separator)
+		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
