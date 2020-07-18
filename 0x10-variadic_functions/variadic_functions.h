559fe3f85bc4dc1bbef14823c39683bb93ff3b8f
Athesto <gamez.live@gmail.com>
2020-07-18 20:25:37 +0000

    HEADE 0x10

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _VAGRANT_H_
+#define _VAGRANT_H_
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* _VAGRANT_H_ */
