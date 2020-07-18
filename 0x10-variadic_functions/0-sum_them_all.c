b8845f5a715d4660784a9b174adbf7d7be3c4006
Athesto <gamez.live@gmail.com>
2020-07-18 20:25:58 +0000

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - main
+ * @n: input
+ * Return: 0;
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list lista;
+	int suma = 0;
+	unsigned int i;
+
+	va_start(lista, n);
+	for (i = 0; i < n; i++)
+	{
+		suma += va_arg(lista, int);
+		i++;
+	}
+	va_end(lista);
+	return (suma);
+}
