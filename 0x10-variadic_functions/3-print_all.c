d89b94266871f944aed0ab3a9f9068c9686fac37
Athesto <gamez.live@gmail.com>
2020-07-19 12:18:44 -0500

    3-print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - main print all
+ * @format: string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	const char *pformat = format;
+	int d;
+	char c, *s;
+	double f;
+
+	va_start(ap, format);
+	/* cief */
+	while (*pformat)
+	{
+		switch (*pformat++)
+		{
+			case 'c':              /* char */
+				c = (char) va_arg(ap, int);
+				printf("%c", c);
+				break;
+			case 's':              /* string */
+				s = va_arg(ap, char *);
+				if (s)
+					printf("%s", s);
+				else
+					printf("(nil)");
+				break;
+			case 'i':              /* int */
+				d = va_arg(ap, int);
+				printf("%d", d);
+				break;
+			case 'f':              /* float */
+				f = va_arg(ap, double);
+				printf("%f", f);
+				break;
+			default:
+				continue;
+		}
+		if (*(pformat))
+			printf(", ");
+		else
+			printf("\n");
+	}
+	va_end(ap);
+}
