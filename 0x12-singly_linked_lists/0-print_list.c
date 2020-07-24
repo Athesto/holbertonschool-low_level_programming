f21a12e011ce7887770e73d726e8fd7442f0650d
Athesto <gamez.live@gmail.com>
2020-07-23 21:59:05 -0500

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,10 +3,17 @@
 /**
  * print_list - print list of elements
  * @h: input
- * Return: 0
+ * Return: Number of Nodes
  */
 size_t print_list(const list_t *h)
 {
-	(void) h;
-	return (0);
+	if (h->str)
+		printf("[%d] %s%s", h->len, h->str, "\n");
+	else
+		printf("[%d] %s%s", 0, "(nil)", "\n");
+	if (h->next)
+	{
+		return (1 + print_list(h->next));
+	}
+	return (1);
 }
