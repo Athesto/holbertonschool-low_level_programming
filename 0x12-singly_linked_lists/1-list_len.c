b1c72fbe6c2daa4d015317c57b5fb7f350e91da1
Athesto <gamez.live@gmail.com>
2020-07-23 22:13:16 -0500

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len - len
+ * list_len - list len
  * @h: list
- * Return: ?
+ * Return: number of elements in a linked
  */
 size_t list_len(const list_t *h)
 {
-	(void) h;
+	if (h)
+	{
+		if (h->next)
+			return (1 + list_len(h->next));
+		return (1);
+	}
 	return (0);
-
 }
