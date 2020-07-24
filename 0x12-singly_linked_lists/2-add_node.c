6de05caae9883db36066ca3bc7e867bc60c90a83
Athesto <gamez.live@gmail.com>
2020-07-23 23:13:44 -0500

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,16 +1,29 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node - ??
- * @head: ??
- * @str: ??
+ * add_node - Add node to the list
+ * @head: head of list
+ * @str: new str
  * Return: 0
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t *head[], const char *str)
 {
-	(void) head;
-	(void) str;
+	list_t *new;
 
-	return (0);
-
+	new = malloc(sizeof(list_t));
+	if (new)
+	{
+		new->str = strdup(str);
+		if (new->str)
+		{
+			new->len = strlen(new->str);
+			new->next = *head;
+			*head = new;
+			return (new);
+		}
+		free(new);
+		return (NULL);
+	}
+	return (NULL);
 }
