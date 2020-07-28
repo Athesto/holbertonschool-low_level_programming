2efa58bde8f198fb637c1278e02765d8da1ab5b7
Athesto <gamez.live@gmail.com>
2020-07-28 18:40:44 -0500

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -?
- * @head: ?
- * @index: ?
- * Return: ?
+ * get_nodeint_at_index - get node in index `index`
+ * @head: list
+ * @index: index of node
+ * Return: Pointer to node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	(void)head;
-	(void)index;
+	if (head)
+	{
+		while (index)
+		{
+			head = head->next;
+			index--;
+		}
+		return (head);
+	}
 	return (NULL);
-
 }
