cb54f447ed6109011d87b8f17b8a5463d10bb87f
Athesto <gamez.live@gmail.com>
2020-07-28 01:22:20 -0500

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,23 @@
 #include "lists.h"
 
 /**
- * print_listint - ?
- * @h: ?
- * Return: ?
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head pointer
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	(void)h;
-	return (0);
+	const listint_t *ptr = h;
+	int elements = 0;
+
+	if (ptr)
+	{
+		while (ptr)
+		{
+			printf("%d\n", (*h).n);
+			elements++;
+			ptr = (*ptr).next;
+		}
+	}
+	return (elements);
 }
