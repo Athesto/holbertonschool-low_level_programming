a6d33157642bba0a31043b97196c169f71094171
Athesto <gamez.live@gmail.com>
2020-07-30 14:55:31 -0500

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
+#define INT_BITS (8 * 4)
 
 /**
- * print_binary - ?
- * @n: ?
- * Return: ?
+ * print_binary - print binary format
+ * @n: number
+ * Return: print binary
  */
 void print_binary(unsigned long int n)
 {
-	(void)n;
+	int b, b_idx;
+	unsigned int MSB1 = 0;
+
+	for (b_idx = INT_BITS; b_idx >= 1; b_idx--)
+	{
+		b = n >> (b_idx - 1);
+		b = b & 1;
+		MSB1 = MSB1 | b;
+		if (MSB1 || b_idx == 1)
+			_putchar('0' + b);
+	}
 }
