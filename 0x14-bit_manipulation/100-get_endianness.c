777724c083a0dfd0a7e886201738618f8c2d3d2e
Athesto <gamez.live@gmail.com>
2020-07-30 17:16:33 -0500

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * get_endianness - ?
- * Return: ?
+ * get_endianness - check if the number is endianess
+ * Return: 1 if it edianess 0 otherwise
  */
 int get_endianness(void)
 {
-	return (0);
+	char c = '@';
+	int x = 0x0001;
+	char *p = &c;
+
+	if (*(p + 1))
+		return (1);
+	else
+		return (0);
+	(void)x;
 }
