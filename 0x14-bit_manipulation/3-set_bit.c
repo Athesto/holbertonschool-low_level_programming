59f461afafcb377c347fba0b17539e321301c706
Athesto <gamez.live@gmail.com>
2020-07-30 15:57:28 -0500

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
+#define LONG_BITS (sizeof(long int) * 8)
 
 /**
- * set_bit - ?
- * @n: ?
- * @index: ?
- * Return: ?
+ * set_bit - set bit at index in n
+ * @n: input
+ * @index: index to set
+ * Return: 1 if worked or -1
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	(void)n;
-	(void)index;
-	return (0);
+	if (n)
+	{
+		*n |= 1 << (index);
+		return (1);
+	}
+	return (-1);
 }
