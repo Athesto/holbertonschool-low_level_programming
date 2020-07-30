321c5b2ba1f9c1a8cc889aa9eb24d0bf5f53cc0a
Athesto <gamez.live@gmail.com>
2020-07-30 14:34:24 -0500

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,29 @@
 #include "holberton.h"
+#include <string.h>
 
 /**
- * binary_to_uint - ?
- * @b: ?
- * Return: ?
+ * binary_to_uint - converts b into binary
+ * @b: string with binary num
+ * Return: b converted or 0 if there is an ivalid char, or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	(void)b;
+	const char *ptr = b;
+	unsigned int number;
+
+	/* b exist */
+	if (ptr)
+	{
+		/* len of ptr && check if str is valid */
+		number = 0;
+		while (*ptr == '0' || *ptr == '1' || ptr == NULL)
+		{
+			number += *ptr - '0';
+			ptr++;
+			if (!*ptr)
+				return (number);
+			number = number << 1;
+		}
+	}
 	return (0);
 }
