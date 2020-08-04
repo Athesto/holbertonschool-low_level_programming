114e322a2338b85e289039b140d5bd34dce96f74
Athesto <gamez.live@gmail.com>
2020-08-04 18:41:47 -0500

    checkout to 0197464

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * read_textfile - read a file and print it in stdout
  * @filename: path of file
@@ -8,19 +9,26 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	int o_read, o_write, fd;
+	int o_read, o_w, fd;
 
-	buf = malloc(sizeof(*buf) * letters);
-	if (!filename || !buf)
+	o_read = o_w = fd = 0;
+	if (filename)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd != -1)
+		{
+			buf = malloc(letters);
+			if (buf)
+			{
+				o_read = read(fd, buf, letters);
+				if (o_read != -1)
+					o_w = write(STDOUT_FILENO, buf, o_read);
+				free(buf);
+			}
+			close(fd);
+		}
+	}
+	if (fd == -1 || o_read == -1 || o_w == -1)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-	o_read = read(fd, buf, letters);
-	o_write = write(STDOUT_FILENO, buf, o_read);
-	close(fd);
-
-	if (fd == -1 || o_read == -1 || o_write == -1)
-		return (free(buf), 0);
-	free(buf);
-	return (o_write);
+	return (o_w);
 }
