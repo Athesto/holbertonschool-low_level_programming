c7941af30c75f803d6da0be523091d0947d96e99
Athesto <gamez.live@gmail.com>
2020-08-04 19:53:29 -0500

    append message

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (isok);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (fd != -1)
 	{
 		if (text_content)
