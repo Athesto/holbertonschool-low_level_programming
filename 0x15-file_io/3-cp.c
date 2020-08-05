14babb09b0737a7b0a691d05e48dc6f81cd5e646
Athesto <gamez.live@gmail.com>
2020-08-05 01:03:20 -0500

    inital idea

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#define BUF1024 (1024)
 /**
  * main - cp program
  * @argc: argument counter
@@ -7,21 +8,44 @@
  */
 int main(int argc, char *argv[])
 {
-	const char *err_msg[] = {
-		"Error: Can't read from file %s\n",
-		"Error: Can't write to %s\n",
-		"Error: Can't close fd %d\n",
-		NULL
-	};
+	int fd1, fd2, f_close, f_read = -1, f_write = -1;
+	char buf[BUF1024];
 
-	printf("%d\n", argc);
-
+	/* check command input */
 	if (argc != 3)
 	{
 		printf("Usage: cp file_from file_to\n");
 		return (97);
 	}
-	(void) argv;
-	(void) err_msg;
+
+	/* check files */
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd2 == -1)
+	{
+		f_close = close(fd1);
+		if (f_close == -1)
+		{
+			printf("Error: Can't close fd %d\n", fd1);
+			return (100);
+		}
+		printf("Error: Can't write to %s\n", argv[2]);
+		return (99);
+	}
+	while (f_read)
+	{
+		f_read = read(fd1, buf, BUF1024);
+		f_write = write(STDOUT_FILENO, buf, f_read);
+	}
+	if (f_write == -1)
+	{
+		printf("Error: Can't write to %s\n", argv[2]);
+		return (99);
+	}
 	return (0);
 }
