11a2be2c01b54ab9095914ad92dbbad4e728ba88
Athesto <gamez.live@gmail.com>
2020-09-06 22:09:33 -0500

    7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: reference to list
+ * @idx: index of the new node
+ * @n: value in list
+ * Return: node added
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node; /* new node */
+	dlistint_t *runner; /* runner in the list */
+	unsigned int r_idx = 0; /* runner index */
+
+	/* check if it's a valid list*/
+	if (h)
+	{
+		/* check if node has memory */
+		node = malloc(sizeof(*node));
+		if (node)
+		{
+			node->prev = NULL;
+			node->next = NULL;
+			node->n = n;
+			for (runner = *h; r_idx < idx; r_idx++)
+			{
+				runner = runner->next;
+				/* Check if length of list < idx*/
+				if (!runner)
+				{
+					free(node);
+					return (NULL); /* ERROR */
+				}
+			}
+			/* runner->next */
+			node->next = runner->next;
+			node->prev = runner;
+			runner->next = node;
+			/* check if runner is not the last node */
+			if (node->next)
+			{
+				runner = node->next;
+				runner->prev = node;
+			}
+			return (node);
+		}
+		return (NULL); /* ERROR */
+	}
+	return (NULL); /* ERROR */
+}
