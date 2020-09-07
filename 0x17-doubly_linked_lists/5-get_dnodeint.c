fe50504624882a6d3201d9392e971d2dbb8281ae
Athesto <gamez.live@gmail.com>
2020-09-06 20:13:35 -0500

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: list
+ * @index: node index
+ * Return: index of node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int l_idx;
+	/* Check if list is valid */
+	if (head)
+	{
+		for (l_idx = 0; l_idx < index; l_idx++)
+			head = head->next;
+		return (head);
+	}
+	return (NULL);
+}
