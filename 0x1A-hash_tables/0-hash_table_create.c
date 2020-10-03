acbd7e5a3f7c88f9d14e0c3a434baff425d4d213
Athesto <gamez.live@gmail.com>
2020-10-02 20:35:48 -0500

    malloc to size of hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
-	hash_table =  malloc(size * sizeof(*hash_table));
+	hash_table =  malloc(sizeof(*hash_table));
 	if (hash_table == NULL)
 		return (NULL);
+
+	hash_table->array = malloc(size * sizeof(*(hash_table->array)));
+	if (hash_table->array == NULL)
+		return (NULL);
+
 	return (hash_table);
 }
