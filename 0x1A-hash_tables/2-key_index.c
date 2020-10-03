3e931df0b894217b2dbbb587066a7bde7113c16f
Athesto <gamez.live@gmail.com>
2020-10-02 22:05:14 -0500

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - ?
+ * @key: ?
+ * @size: ?
+ * Return: ?
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	(void) key;
+	(void) size;
+	return (0);
+}
