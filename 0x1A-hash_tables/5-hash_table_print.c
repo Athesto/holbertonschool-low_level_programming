e7ec89daba5c692ad49c982bf6dd2a66c427785e
Athesto <gamez.live@gmail.com>
2020-10-04 16:28:44 -0500

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,37 @@
 #include "hash_tables.h"
+#include <stdio.h>
 /**
  * hash_table_print - ?
  * @ht: ?
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	(void) ht;
+	hash_node_t **runner, *list_runner;
+	unsigned long int h_idx;
+	int first_printed = 0;
+
+	printf("{");
+	if (ht)
+	{
+		runner = ht->array;
+		for (h_idx = 0; h_idx <= ht->size; h_idx++)
+		{
+			if (runner[h_idx])
+			{
+				list_runner = runner[h_idx];
+				while (list_runner)
+				{
+					if (first_printed)
+						printf(", ");
+					printf("'%s': '%s'",
+							list_runner->key,
+							list_runner->value
+							);
+					first_printed = 1;
+					list_runner = list_runner->next;
+				}
+			}
+		}
+	}
+	printf("}\n");
 }
