fa8effa6013a2536bf45737df68300c358c67fbc
Athesto <gamez.live@gmail.com>
2020-10-04 13:07:31 -0500

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-
+#include <stdlib.h> /* NULL */
+#include <string.h> /* strcmp */
 /**
  * hash_table_get - ?
  * @ht: ?
@@ -9,7 +9,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	(void) ht;
-	(void) key;
+	unsigned long int h_idx; /* hash index */
+	hash_node_t *checker;
+
+	if (ht == NULL || ht->array == NULL || key == NULL)
+		return (NULL);
+
+	h_idx = key_index((const unsigned char *)key, ht->size);
+	checker = ht->array[h_idx];
+
+	while (checker)
+	{
+		if (strcmp(checker->key, key) == 0)
+			return (checker->value);
+		checker = checker->next;
+	}
 	return (NULL);
 }
