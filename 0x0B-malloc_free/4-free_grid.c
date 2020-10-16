967025193a8178bb76b14910be9947d3e2bf48a7
Athesto <gamez.live@gmail.com>
2020-10-16 12:03:14 -0500

    0x0B.4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,8 +5,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (**grid && height)
-		return;
-	else
-		return;
+	int h_idx; /* height index */
+
+	for (int h_idx = 0; h_idx < height; h_idx++)
+	{
+		free(grid[h_idx]);
+	}
+	free(grid);
 }
