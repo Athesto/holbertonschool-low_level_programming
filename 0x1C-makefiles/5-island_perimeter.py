93d5050344fd1c7e3b7fc3326a363eaeac6587d2
Athesto <gamez.live@gmail.com>
2020-11-07 13:17:51 -0500

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
+'''task 5, perimetro de una isla'''
+
+
+def neighbours(grid, row, col):
+    '''sum neighbours'''
+    height = len(grid)
+    width = len(grid[0])
+    total = 0
+
+    if col > 0:
+        total += grid[row][col - 1]
+    if row > 0:
+        total += grid[row - 1][col]
+    if col < width:
+        total += grid[row + 1][col]
+    if row < height:
+        total += grid[row][col + 1]
+    return total
+
 
 def island_perimeter(grid):
-    return (4)
+    '''find permiter'''
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for row in range(height):
+        for col in range(width):
+            if (grid[row][col]):
+                perimeter = perimeter + 4 - neighbours(grid, row, col)
+    return (perimeter)
